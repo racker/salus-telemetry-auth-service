7fec0125daaa1d6554a405dab5e761ebfa98df0a
Geoff Bourne <geoff.bourne@rackspace.com>
2018-11-16 10:36:41 -0600

    Initial repo split from https://github.com/racker/salus-telemetry-core/commit/599d81a4d70a1f3b4edb19037d8cbf3a790eec0c

---
+++ src/main/java/com/rackspace/salus/authservice/AuthController.java
@@ -0,0 +1,76 @@
+/*
+ *    Copyright 2018 Rackspace US, Inc.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ *
+ */
+
+package com.rackspace.salus.authservice;
+
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.vault.core.VaultTemplate;
+import org.springframework.vault.support.VaultCertificateRequest;
+import org.springframework.vault.support.VaultCertificateResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    private final VaultTemplate vaultTemplate;
+    private final Counter certCounter;
+
+    @Autowired
+    public AuthController(VaultTemplate vaultTemplate,
+                          MeterRegistry meterRegistry) {
+        this.vaultTemplate = vaultTemplate;
+
+        certCounter = meterRegistry.counter("messages","certsAssigned", "stage");
+    }
+
+    @GetMapping("cert")
+    // Note Confirm this is the correct role for this method:
+    @Secured({"ROLE_COMPUTE_DEFAULT"})
+    public ResponseEntity<CertResponse> getCert(@RequestHeader("X-Tenant-Id") String tenant) {
+        final VaultCertificateResponse resp = vaultTemplate.opsForPki()
+                .issueCertificate("telemetry-infra",
+                        VaultCertificateRequest.create(tenant));
+        CertResponse rd = new CertResponse(formatCert(resp.getData().getCertificate(), "CERTIFICATE"),
+                                           formatCert(resp.getData().getIssuingCaCertificate(), "CERTIFICATE"),
+                                           formatCert(resp.getData().getPrivateKey(), "RSA PRIVATE KEY"));
+        certCounter.increment();
+        log.info("Retrieving client certificates for tenant={}", tenant);
+        return ResponseEntity.ok(rd);
+    }
+
+    static String formatCert(String c, String name) {
+        StringBuilder n = new StringBuilder("-----BEGIN " + name + "-----");
+        for (int i = 0; i < c.length() ; i++) {
+            if ((i % 64) == 0) {
+                n.append("\n");
+            }
+            n.append(c.charAt(i));
+        }
+        n.append("\n-----END " + name + "-----");
+        return n.toString();
+    }
+}
---
+++ src/test/java/com/rackspace/salus/authservice/AuthControllerTest.java
@@ -0,0 +1,107 @@
+/*
+ *    Copyright 2018 Rackspace US, Inc.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ *
+ */
+
+package com.rackspace.salus.authservice;
+
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.vault.core.VaultPkiOperations;
+import org.springframework.vault.core.VaultTemplate;
+import org.springframework.vault.support.CertificateBundle;
+import org.springframework.vault.support.VaultCertificateResponse;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class AuthControllerTest {
+
+   @Autowired
+   private MockMvc mvc;
+   @MockBean
+   VaultTemplate vt;
+   @Mock
+   VaultPkiOperations pki;
+   @Mock
+   VaultCertificateResponse cr;
+   @Mock
+   CertificateBundle cb;
+
+   @Test
+   public void getCertSuccessful() throws Exception {
+
+      when(vt.opsForPki()).thenReturn(pki);
+      when(pki.issueCertificate(any(), any())).thenReturn(cr);
+      when(cr.getData()).thenReturn(cb);
+      when(cb.getCertificate()).thenReturn("cert");
+      when(cb.getIssuingCaCertificate()).thenReturn("ica");
+      when(cb.getPrivateKey()).thenReturn("key");
+
+      HttpHeaders h = new HttpHeaders();
+      h.add("X-Roles", "compute:default");
+      h.add("X-Tenant-Id", "123456");
+      mvc.perform(
+               get("/auth/cert")
+                   .headers(h))
+                   .andExpect(status().is(200))
+                   .andExpect(jsonPath("$.certificate",
+                       is("-----BEGIN CERTIFICATE-----\ncert\n-----END CERTIFICATE-----")))
+                   .andExpect(jsonPath("$.issuingCaCertificate",
+                       is("-----BEGIN CERTIFICATE-----\nica\n-----END CERTIFICATE-----")))
+                   .andExpect(jsonPath("$.privateKey",
+                       is("-----BEGIN RSA PRIVATE KEY-----\nkey\n-----END RSA PRIVATE KEY-----")));
+   }
+
+   @Test
+   public void getCertBadRole() throws Exception {
+      HttpHeaders h = new HttpHeaders();
+      h.add("X-Roles", "compute:not-default");
+      h.add("X-Tenant-Id", "123456");
+      mvc.perform(
+              get("/auth/cert")
+                  .headers(h))
+                  .andExpect(status().is(403));
+   }
+
+   @Test
+   public void getCertNoTenant() throws Exception {
+      HttpHeaders h = new HttpHeaders();
+      h.add("X-Roles", "compute:default");
+      mvc.perform(
+              get("/auth/cert")
+                  .headers(h))
+                  .andExpect(status().is(403));
+   }
+}
+
